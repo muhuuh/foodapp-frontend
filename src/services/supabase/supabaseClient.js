6a7979198b44ef1b071257849ff48ae69eac3428
arani <aranimontes@protonmail.com>
2024-07-06 14:11:21 +0200

    basic app setup

---
+++ src/services/supabase/supabaseClient.js
@@ -0,0 +1,13 @@
+// supabase.js
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY;
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+  auth: {
+    persistSession: true,
+  },
+});
+
+export default supabase;
