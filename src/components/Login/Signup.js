738c3b0f76c3e7b153accf9354f6fa2ca8392042
arani <aranimontes@protonmail.com>
2024-07-07 15:40:11 +0200

    password and email validity checks

--- src/components/Login/Signup.js
+++ src/components/Login/Signup.js
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../../services/supabase/authContext";
-import supabase from "../../services/supabase/supabase";
 
 const SignUp = () => {
   const { signUp, signIn } = useContext(AuthContext);
@@ -8,8 +7,39 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
 
+  const validateEmail = (email) => {
+    const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const validatePassword = (password) => {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasDigit = /[0-9]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    return (
+      password.length >= minLength &&
+      hasUpperCase &&
+      hasLowerCase &&
+      hasDigit &&
+      hasSpecialChar
+    );
+  };
+
   const handleSignUp = async (e) => {
     e.preventDefault();
+    if (!validateEmail(email)) {
+      setMessage("Invalid email address");
+      return;
+    }
+    if (!validatePassword(password)) {
+      setMessage(
+        "Password muss mindestens 8 Zeichen lang sein und jeweils mindestens ein Großbuchstaben, eine Zahl und ein Sonderzeichen."
+      );
+      return;
+    }
+
     const { user, error } = await signUp(email, password);
     if (error) {
       setMessage(`Error: ${error.message}`);
@@ -44,7 +74,6 @@ const SignUp = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-
         <button type="submit">Sign Up</button>
       </form>
       {message && <p>{message}</p>}
